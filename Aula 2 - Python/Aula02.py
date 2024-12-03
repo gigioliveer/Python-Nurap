ab96befcaf01154153331f2ed5f9c64f0e344aee
gigioliveer <oliveirageovanna749@gmail.com>
2024-12-03 11:24:57 -0300

    Add files via upload

---
+++ Aula 2 - Python/Aula02.py
@@ -0,0 +1,8 @@
+print('Bom dia Carlos')
+
+nome_usuario = 'Geovanna Oliveira' # tipo -> str = string
+idade_usuario = 18 # tipo -> int = integer
+altura_usuario = 1.68 # tipo -> float = ponto flutuante
+maior_idade = True # tipo -> bool = boolean 
+
+print('Seja Bem-Vindo',nome_usuario, 'sua idade é',idade_usuario, 'anos,voce tem', altura_usuario, 'metros. Voce é maior de idade?', maior_idade)
